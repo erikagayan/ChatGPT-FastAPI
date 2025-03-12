0d0f06b04281286a4e82956e68fcd4ec01100726
Erik <erik.agayan.work@gmail.com>
2025-03-13 01:50:24 +0200

    Add models.py

---
+++ database/models.py
@@ -0,0 +1,11 @@
+from database.engine import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    telegram_id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False, index=True)
+    username: Mapped[str | None] = mapped_column(String(50), nullable=True)
+    full_name: Mapped[str | None] = mapped_column(String(100), nullable=True)
