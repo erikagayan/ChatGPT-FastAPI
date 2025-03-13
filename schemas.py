8945f441f4287da752c9a1497511985a940a0891
Erik <erik.agayan.work@gmail.com>
2025-03-13 02:04:08 +0200

    Add schemas.py and new requirements.txt

---
+++ schemas.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class UserCreate(BaseModel):
+    telegram_id: int
+    username: str | None = Field(None, max_length=50)
+    full_name: str | None = Field(None, max_length=100)
+
+class UserResponse(BaseModel):
+    id: int
+    telegram_id: int
+    username: str | None = None
+    full_name: str | None = None
+
+    class Config:
+        from_attributes = True
