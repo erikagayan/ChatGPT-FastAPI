074b82add58e797f0927e7c21ea6f03088669b89
Erik <erik.agayan.work@gmail.com>
2025-03-18 18:58:13 +0200

    start button register user in db

--- database/models.py
+++ database/models.py
@@ -1,11 +1,12 @@
 from database.engine import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Integer, String, BigInteger
+
 
 class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    telegram_id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False, index=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False, index=True)
     username: Mapped[str | None] = mapped_column(String(50), nullable=True)
     full_name: Mapped[str | None] = mapped_column(String(100), nullable=True)
--- tg_bot/handlers/start.py
+++ tg_bot/handlers/start.py
@@ -1,14 +1,29 @@
 """
-Handler that greets the user and displays his full name
+Handle /start command and register user in the database
 """
 
 from aiogram import types
 from tg_bot.handlers import router
 from aiogram.filters import Command
 
+import aiohttp
+from tg_bot.config import BACKEND_URL
+
 
 @router.message(Command("start"))
 async def start_handler(message: types.Message):
-    """Handle /start command and send a simple greeting"""
-    user_name = message.from_user.full_name
-    await message.answer(f"Hello, {user_name}!")
+    """Handle /start command and register user in the database"""
+    user_data = {
+        "telegram_id": message.from_user.id,
+        "username": message.from_user.username,
+        "full_name": message.from_user.full_name
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{BACKEND_URL}/api/users/", json=user_data) as response:
+            if response.status in {200, 201}:
+                user = await response.json()
+                await message.answer(
+                    f"✅ Welcome, {user['full_name'] or user['username'] or 'User'}!\nYour ID: {user['id']}")
+            else:
+                await message.answer("❌ Registration error. Please try again later.")
